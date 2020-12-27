7101a00b98d2ea3857b571cae867fc8ea0216803
Sam Vervaeck <samvv@pm.me>
2020-12-27 15:26:57 +0100

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+
+const path = require('path');
+
+const packageDir = path.resolve(__dirname);
+
+module.exports = {
+  entry: './src/examples.tsx',
+  mode: 'development',
+  output: {
+    filename: 'examples.bundle.js',
+    path: packageDir,
+  },
+  devServer: {
+    host: '0.0.0.0',
+    historyApiFallback: true,
+  },
+  devtool: 'eval-cheap-module-source-map',
+  target: 'web',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@emotion/babel-plugin',
+            ],
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
+          }
+        }
+      }
+    ]
+  }
+};
+
